8ad2a2fa208f974646d62f67cb62f3d737b0b003
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2026-01-25 08:41:40 +0100

    update neovim config

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -9,15 +9,6 @@ return {
   },
 
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
-
-  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
